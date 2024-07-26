ecf97ae2d2bde8de3834baadbaec7c094762583c
Dilukshan <dilukshan2020da@gmail.com>
2024-07-26 17:56:27 +0530

    6th commit - add app.use(cors()

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,12 @@ const { type } = require("os");
 const { error } = require("console");
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin:'https://viduna-web-frontend.vercel.app/',
+    methods:['GET','POST','PUT','DELETE'],
+    credentials:true,
+    allowedHeaders:['Content-Type','auth-token']
+}));
 
 
 
